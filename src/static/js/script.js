14f7b6599e025389de2a3275e979cb284b13bff0
João Pereira <joaopereira@criticalmanufacturing.com>
2022-06-03 13:19:11 +0100

    chore: add modal on img click

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,8 @@
-$('.nav-link').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+$(".nav-link").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
+
+$(".pop").on("click", function () {
+  $(".imagepreview").attr("src", $(this).find("img").attr("src"));
+  $("#imagemodal").modal("show");
 });
